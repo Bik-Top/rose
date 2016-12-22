c0b13e77feab0e30eb0a2e92c450e981439c286d
BikTop <mantorovv@gmail.com>
2016-12-22 02:36:07 +0200

    article

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'  # запросы к корню
+  #root 'welcome#index' # в экшн index контроллера welcome
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :articles
+  root 'welcome#index'
 end
